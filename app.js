059e9e02365a5f3664176a2da1f7dd76701825ff
Gabriel Xavier <gabrielx4000@hotmail.com>
2022-09-17 23:12:15 -0300

    Add smoothies endpoint

--- app.js
+++ app.js
@@ -6,10 +6,38 @@ const PORT = process.env.PORT
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/hello', (req, res) => {
-    res.json({ "hello": "world" })
+const fs = require('fs')
+
+const smoothies = JSON.parse(fs.readFileSync('smoothies.json'))
+
+app.get('/smoothies', (req, res) => {
+    let response = []
+
+    const titleSort = ((a, b) => {
+        let x = a.title
+        let y = b.title
+
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+    })
+
+    if (req.query.title != undefined) {
+        response = smoothies.filter((smoothie) => { return smoothie.title == req.query.title }).sort(titleSort)
+        
+    } else {
+        response = smoothies.map((smoothie) => {
+            return {
+                title: smoothie.title,
+                url: smoothie.url,
+                thumbnail: smoothie.thumbnail,
+                tags: smoothie.tags,
+                rating: smoothie.rating
+            }
+        }).sort(titleSort)
+    }
+    
+    res.json(response)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running...`);
+    console.log(`Server running at http://localhost:${PORT}`);
 })
